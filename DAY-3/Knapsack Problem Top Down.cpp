b52e0ea4ec7be31baeff7ab263e12f4e3ed7eb5c
Dhruv Kothari <kotharidhruv25@gmail.com>
2020-10-07 22:58:51 +0530

    Update Knapsack Problem Top Down.cpp

--- DAY-3/Knapsack Problem Top Down.cpp
+++ DAY-3/Knapsack Problem Top Down.cpp
@@ -1,44 +1,47 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define lli long long int
+#define endl "\n"
+#define MAX 1005
 using namespace std;
 
 
-
-lli knapsack(lli weight[], lli value[], lli w, lli n)
+int knapsack(int weight[], int value[], int w, int n)
 {
-	lli dp[n+1][w+1];
-
-	for(int i =0; i<=n; i++)
-	{
-		for(int j= 0; j<=w; j++)
-		{
-			//initialisation condition
-			if(i == 0 || j ==0)
-				dp[i][j] = 0;
-			else
-			{
-				if(weight[i-1] <= w)
-					dp[i][j] = max(value[i-1] + dp[i-1][w-weight[i-1]], dp[i-1][w]);
-				else 
-					dp[i][j] = dp[i-1][w];
-			}
-		}
-	}
-	return dp[n][w];
+    int dp[n+1][w+1];
+    
+//initialisation condition
+    for(int i=0;i<n+1; i++)
+        dp[i][0] = 0;
+    for(int i=0;i<w+1; i++)
+        dp[0][i] = 0;
+        
+    for(int i = 1; i<=n; i++)
+    {
+        for(int j = 1; j<=w; j++)
+        {
+            if(weight[i-1] <= j)
+                dp[i][j] = max((value[i-1] + dp[i-1][j-weight[i-1]]), dp[i-1][j]);
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+    return dp[n][w];
 }
 
-
-
 int main()
-{
-
-	lli n,w;
-	cin>>w>>n;
-	lli weight[n], value[n];
-
-	for(lli i =0; i<n; i++)
-		cin>>weight[i]>>value[i];
-	cout<<knapsack(weight, value, w, n);
-	
+ {
+	int t;
+	cin>>t;
+	while(t--)
+	{   
+	    int n, w;
+	    cin>>n>>w;
+	    int weight[n], value[n];
+	    for(int i =0; i<n; i++)
+	        cin>>value[i];
+	    for(int i =0; i<n; i++)
+	        cin>>weight[i];
+	    cout<<knapsack(weight, value, w, n)<<endl;
+	}
 	return 0;
 }
